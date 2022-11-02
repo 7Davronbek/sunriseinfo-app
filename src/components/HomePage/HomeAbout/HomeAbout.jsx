e22995e1837fcc5291e36fec712e4709bf20494e
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-11-02 14:19:36 +0500

    HomePage - media querry

--- src/components/HomePage/HomeAbout/HomeAbout.jsx
+++ src/components/HomePage/HomeAbout/HomeAbout.jsx
@@ -5,16 +5,18 @@ import './homeAbout.css'
 export default function HomeAbout() {
   return (
     <>
-    <div className="home_about">
-        <div data-aos="fade-right" data-duration-aos="3000" className="text_home">
+      <div className="home_about">
+        <div className="about_container">
+          <div data-aos="fade-right" data-duration-aos="3000" className="text_home">
             <h3>Sun Rise</h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
             <ul className='takrorlanmas'>
-                <Link style={{textDecoration: "none", color: "#ffffff"}} to="/contact"><li>Contact us</li></Link>
-                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Viverra sed eget facilisi at iaculi. </li>
+              <Link style={{ textDecoration: "none", color: "#ffffff" }} to="/contact"><li>Contact us</li></Link>
+              <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Viverra sed eget facilisi at iaculi. </li>
             </ul>
+          </div>
         </div>
-    </div>
+      </div>
     </>
   )
 }
